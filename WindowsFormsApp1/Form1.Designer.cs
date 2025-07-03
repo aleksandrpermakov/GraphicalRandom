8a4c5920e39d903ec8e3d861e6a4fe53bfe2d55a
User <User@WIN-7RSVVMUHAJG>
2025-07-03 19:19:33 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.tbX_location = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbX_location
+            // 
+            this.tbX_location.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbX_location.Location = new System.Drawing.Point(24, 363);
+            this.tbX_location.Name = "tbX_location";
+            this.tbX_location.Size = new System.Drawing.Size(240, 45);
+            this.tbX_location.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbX_location);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbX_location;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+        TextBox tb; 
+		public Form1()
+		{
+			InitializeComponent();
+			CreetyTextBox(tbX_location); 
+
+		}
+		public void CreetyTextBox(TextBox tb)
+		{
+			TextBox tbY = new TextBox();
+			tbY.Location = new Point(tb.Size.Width + 200, tb.Location.Y);
+			tbY.Size = tbX_location.Size;
+			tbY.TabIndex = tbX_location.TabIndex +1;
+            tbY.Name = "tbY";
+            tbY.Text = tb.Location.Y.ToString();
+			tbY.Font = tb.Font;
+            Controls.Add(tbY);
+			this.tb = tbY;
+
+
+        }
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			tbX_location.Text =  e.Location.X.ToString();
+			tb.Text = e.Location.Y.ToString();
+		}
+	}
+}
